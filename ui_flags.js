d16977324bbf4de6e5317ae86cadc85b304bbf54
Mark Darby <markcdarby@gmail.com>
2025-10-29 14:52:42 -0600

    feat(cards): add experimental Tailwind-first V5 card variants behind UI_FLAGS.twRevamp; bump cache; update README flag docs

--- ui_flags.js
+++ ui_flags.js
@@ -2,5 +2,6 @@
 window.UI_FLAGS = Object.assign({}, window.UI_FLAGS || {}, {
   // Enable new Stream/Mosaic card rendering in List/Grid views
   cardV4: true,
+  // Experimental Tailwind-first card/layout revamp (v5)
+  twRevamp: true,
 });
-
